9685fe8656edb4f0a214e814dd48ac008a1d6b2e
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-08-29 12:17:40 +0545

    feat:added a route and handle the multer

---
+++ media-service/src/routes/media-route.js
@@ -0,0 +1,52 @@
+import express from "express";
+import multer from "multer";
+import { uploadMedia } from "../controllers/media-controller.js";
+import { authenticateRequest } from "../middlewares/auth-middleware.js";
+import logger from "../utils/logger.js";
+
+const router = express.Router();
+
+//configure multer for file upload
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+}).single("file");
+
+router.post(
+  "/upload",
+  authenticateRequest,
+  (req, res, next) => {
+    upload(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        logger.error(`Multer error: ${err.message}`);
+        return res.status(400).send({
+          success: false,
+          message: "Multer error: " + err.message,
+          error: err.message,
+          stack: err.stack,
+        });
+      } else if (err) {
+        logger.error(`Unknown error: ${err.message}`);
+        return res.status(500).send({
+          success: false,
+          message: "Unknown error: " + err.message,
+          error: err.message,
+          stack: err.stack,
+        });
+      }
+      if (!req.file) {
+        logger.error("No file found. Please add a file and try again!");
+        return res.status(400).send({
+          success: false,
+          message: "No file found. Please add a file and try again!",
+        });
+      }
+      next();
+    });
+  },
+  uploadMedia
+);
+
+export default router;
